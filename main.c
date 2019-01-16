6849a5545c2e6fa8316cf8a2ef338decf6553e90
Your Name <you@example.com>
2019-01-16 11:44:56 +0800

    敏感字符管理系统

--- main.c
+++ main.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
 
-int main(){
 
-    printf("hello world!");
+char arr[1024];
+int index = 0;
 
+int main()
+{
+    int code = 0;
+
+    printf("minganzifuguanlixitong\n");
+
+    while (1){
+
+    scanf("%d",&code);
+
+    if(code == 1){
+        printf("qingshuruyaotianjiademinganzifu\n");
+        char m;
+        scanf("%c",&m);
+        scanf("%c",&m);
+
+        arr[index] = m;
+        index++;
+
+        printf("tianjiachenggong,dainjihuichejixu\n");
+        char x;
+        scanf("%c",&x);
+        scanf("%c",&x);
+    }
+
+    if(code == 2){
+
+    }
+    if(code == 3){
+        
+    }
+    if(code == 4){
+        
+    }
+    if(code == 5){
+        printf("tuichuxitong\n");
+        break;
+    }
+
+    }
+    
     return 0;
-
+    
 }
